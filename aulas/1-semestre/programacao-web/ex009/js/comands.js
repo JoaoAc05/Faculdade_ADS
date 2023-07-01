4e22b3de59c7f5853af10c21bf4db3025bb33460
João Alves <joaoalvescordeiro2005@gmail.com>
2023-07-01 15:51:54 -0300

    finalização
    Ajustes do projeto, complementação do linkedin, melhoria na responsividade, organização do código...

---
+++ aulas/1-semestre/programacao-web/ex009/js/comands.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var nomeElement = document.getElementById('my-name');
+    var nome = 'João Alves';
+    var index = 0;
+
+    function escreverNome() {
+        nomeElement.innerHTML += nome[index];
+        index++;
+        if (index < nome.length) {
+        setTimeout(escreverNome, 200); 
+        }
+    }
+
+    escreverNome();
+    });
+    
+
+    function scrollToDiv(targetId) {
+    const targetElement = document.getElementById(targetId);
+    const targetPosition = targetElement.offsetTop - 80; // Subir 80 pixels do header
+    const startPosition = window.scrollY;
+    const distance = targetPosition - startPosition;
+    const duration = 1500; // 1,5 segundos
+    let start = null;
+    function animateScroll(timestamp) {
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+        if (progress < duration) {
+        requestAnimationFrame(animateScroll);
+        }
+    }
+
+
+    function easeInOutCubic(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t * t + b;
+        t -= 2;
+        return c / 2 * (t * t * t + 2) + b;
+    }
+    requestAnimationFrame(animateScroll);
+    }
